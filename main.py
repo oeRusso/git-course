c5298e853ff33e9db86a031dbe5dbae67fc2c4d0
Esteban Russo <oerusso28@gmail.com>
2022-09-01 11:55:16 -0300

    desarrollo: creando logica en los archivos raices. BC

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.app import sum
+
+def main() ->None:
+    sum(5,10)
+    
+    if __name__ == '__main__':
+        main()
+        
+        
+        # hasta ahora solo modifique los archivos y hice un git add . nada mas. retomar en este punto mañana
